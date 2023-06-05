f518fbe051d3bcacd60d89073999a3ce5ef8df53
samsoe <esamsoe@gmail.com>
2023-06-05 14:25:19 -0600

    test_sleep

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #! bin/bash
 
+echo "Sleeping for 60 seconds to allow VM to initialize..." > /tmp/sleep.log
+
+sleep 1800
+
+echo "Finished sleeping for 1800 seconds." >> /tmp/sleep.log
+
 # Transfer hello_world.txt to cloud storage
 gsutil cp /usr/local/bin/datasets/hello_world.txt gs://mpg-aerial-survey/automation/output_ortho/ > /tmp/gsutil.log 2>&1
 
