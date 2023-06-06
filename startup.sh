fc38d78d398861e24515a30c39dac788567f3bce
samsoe <esamsoe@gmail.com>
2023-06-06 13:36:23 -0600

    Remove sleep from startup

--- startup.sh
+++ startup.sh
@@ -1,15 +1,3 @@
 #! bin/bash
 
-echo "Sleeping for 60 seconds to allow VM to initialize..." > /tmp/sleep.log
-
-sleep 1800
-
-echo "Finished sleeping for 1800 seconds." >> /tmp/sleep.log
-
-# Transfer hello_world.txt to cloud storage
-gsutil cp /usr/local/bin/datasets/hello_world.txt gs://mpg-aerial-survey/automation/output_ortho/ > /tmp/gsutil.log 2>&1
-
-# Print dialogue for debugging
-if [ $? -ne 0 ]; then
-    echo "gsutil command failed. Check /tmp/gsutil.log for details."
-fi
+echo "Startup Complete" > /tmp/startup.log
