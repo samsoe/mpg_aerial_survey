296e99452e73612cba1189bc2009e4096803327a
Kyle Doherty <kdoherty@MPG-KyleDoherty.local>
2023-06-07 15:58:12 -0700

    fix for loop

--- surveys/230601_spurgepoly/run_job.sh
+++ surveys/230601_spurgepoly/run_job.sh
@@ -2,8 +2,6 @@
 
 url=https://raw.githubusercontent.com/samsoe/mpg_aerial_survey/main/surveys/230601_spurgepoly/config_file.json
 
-for idx in for idx in {0..9};
-do
+for idx in {0..9};
     gcloud compute instances add-metadata odm-array-$idx --zone us-central1-a --metadata array_idx=$idx,config_url=$url
     gcloud compute instances start odm-array-$idx
-done
