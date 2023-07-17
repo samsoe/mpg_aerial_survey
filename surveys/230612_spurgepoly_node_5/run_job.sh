9b48adbb77de6e19342c40f9713628a08a94b01e
Kyle Doherty <kdoherty@MPG-KyleDoherty.local>
2023-07-17 09:30:00 -0700

    prep no orthophoto

--- surveys/230612_spurgepoly_node_5/run_job.sh
+++ surveys/230612_spurgepoly_node_5/run_job.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-url=https://raw.githubusercontent.com/samsoe/mpg_aerial_survey/main/surveys/230612_spurgepoly/config_file.json
+url=https://raw.githubusercontent.com/samsoe/mpg_aerial_survey/main/surveys/230612_spurgepoly_node_5/config_file.json
 idx=5 #runnin job 5 only to test no orthophoto
 gcloud compute instances add-metadata odm-array-$idx --zone us-central1-a --metadata array_idx=$idx,config_url=$url
 gcloud compute instances start odm-array-$idx
