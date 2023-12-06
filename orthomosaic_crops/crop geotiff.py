ae8a5239e832414d17507d0cbee80ee928091d5b
samsoe <esamsoe@gmail.com>
2023-12-06 11:03:46 -0700

    Combine crop and save functions

--- orthomosaic_crops/crop geotiff.py
+++ orthomosaic_crops/crop geotiff.py
@@ -30,60 +30,38 @@ def get_bounding_boxes(gdf, side_len=1):
     return gpd.GeoDataFrame(geometry=bounding_boxes, crs=gdf.crs)
 
 
-def crop_geotiff(geotiff_path, bounding_boxes):
+def crop_and_save_geotiff(
+    geotiff_path, bounding_boxes, output_folder, write_images=True, prefix="cropped"
+):
     """
-    Crops the GeoTIFF based on the bounding boxes and returns the cropped images.
+    Crops the GeoTIFF based on the bounding boxes.
+    If write_images is True, saves each cropped image immediately.
     """
-    cropped_images = []
-    with rasterio.open(geotiff_path) as src:
-        for bbox in bounding_boxes.geometry:
-            # Create a GeoDataFrame for the bounding box
-            bbox_gdf = gpd.GeoDataFrame(geometry=[bbox], crs=bounding_boxes.crs)
-
-            # Transform the bounding box to the GeoTIFF's coordinate system
-            bbox_transformed = bbox_gdf.to_crs(src.crs)
-
-            # Crop the image
-            out_image, out_transform = mask(
-                src, [bbox_transformed.geometry.iloc[0]], crop=True
-            )
-            cropped_images.append(out_image)
-
-    return cropped_images
-
-
-def save_cropped_images(cropped_images, output_folder, geotiff_path, prefix="cropped"):
-    """
-    Saves the list of cropped images to the specified folder.
-    """
-    # Create the output folder if it doesn't exist
-    if not os.path.exists(output_folder):
+    if write_images and not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-    # Open the original GeoTIFF to get its metadata
     with rasterio.open(geotiff_path) as src:
         meta = src.meta.copy()
 
-    # Loop through the cropped images and save each
-    for i, image in enumerate(cropped_images):
-        # Update the metadata for each image
-        meta.update(
-            {
-                "driver": "GTiff",
-                "height": image.shape[1],
-                "width": image.shape[2],
-                "transform": from_origin(
-                    0, 0, 1, 1
-                ),  # Update with correct transform if available
-            }
-        )
+        for i, bbox in enumerate(bounding_boxes.geometry):
+            bbox_gdf = gpd.GeoDataFrame(geometry=[bbox], crs=bounding_boxes.crs)
+            bbox_transformed = bbox_gdf.to_crs(src.crs)
 
-        # Define the output path
-        output_path = os.path.join(output_folder, f"{prefix}_{i}.tif")
+            out_image, out_transform = mask(
+                src, [bbox_transformed.geometry.iloc[0]], crop=True
+            )
 
-        # Write the cropped image to a file
-        with rasterio.open(output_path, "w", **meta) as dest:
-            dest.write(image)
+            meta.update(
+                {
+                    "driver": "GTiff",
+                    "height": out_image.shape[1],
+                    "width": out_image.shape[2],
+                    "transform": out_transform,
+                }
+            )
+            if write_images:
+                output_path = os.path.join(output_folder, f"{prefix}_{i}.tif")
+                with rasterio.open(output_path, "w", **meta) as dest:
+                    dest.write(out_image)
 
 
 shapefile_path = "./points.shp"
@@ -92,5 +70,4 @@ output_folder = "./folder/"
 
 points_gdf = load_shapefile(shapefile_path)
 bounding_boxes = get_bounding_boxes(points_gdf)
-cropped_images = crop_geotiff(geotiff_path, bounding_boxes)
-save_cropped_images(cropped_images, output_folder, geotiff_path)
+crop_and_save_geotiff(geotiff_path, bounding_boxes, output_folder)
